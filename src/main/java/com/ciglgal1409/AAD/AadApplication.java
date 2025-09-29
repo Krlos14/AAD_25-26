fd7e4e1462e81a5d991945ab32d93d186b7fb218
Carlos <ciglgal1409@g.educaand.es>
2025-09-29 12:58:01 +0200

    Borro fichero

---
+++ src/main/java/com/ciglgal1409/AAD/AadApplication.java
@@ -0,0 +1,26 @@
+package com.ciglgal1409.AAD;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public
+class
+AadApplication
+        implements
+        CommandLineRunner
+{
+    public static void main(String[] args)
+    {
+             SpringApplication.run(AadApplication.class,args);
+    }
+@Override
+public void run ( String... args ) throws Exception
+    {
+
+        log.info("Lombok ON");
+    }
+}
---
+++ src/test/java/com/ciglgal1409/AAD/AadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciglgal1409.AAD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
